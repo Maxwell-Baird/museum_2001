332d5d70a9e8730c10a57cd5cd08593055df1903
Maxwell Baird <bairdm71@gmail.com>
2020-02-19 10:30:21 -0700

    created the test check annouce winner

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/exhibit'
 require './lib/patron'
 require './lib/museum'
@@ -112,6 +113,25 @@ class MuseumTest < Minitest::Test
   end
 
   def test_museum_announce_lottery_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 20})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 15)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    dmns.stubs(:draw_lottery_winner).return(patron_3)
+    assert_equal 'Johnny has won the Dead Sea Scrolls exhibit lottery', dmns.announce_lottery_winner
   end
 
 end
